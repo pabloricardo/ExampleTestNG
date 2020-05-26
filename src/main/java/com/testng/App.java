2016f353e6a9d6a27790fff229c53d8ebcedf157
Pablo <pablo.mg@hotmail.com>
2020-05-25 21:16:31 -0300

    Start example with testNG

---
+++ src/main/java/com/testng/App.java
@@ -0,0 +1,24 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    @Test
+    public void firs() {
+        System.out.println("Test01_APP");
+    }
+
+    @Test
+    public void secondTest(){
+        System.out.println("Test02_APP");
+    }
+
+    @Test
+    public void thirdTest(){
+        System.out.println("Test03_APP");
+    }
+}
---
+++ src/main/java/com/testng/test01.java
@@ -0,0 +1,10 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+
+public class test01 {
+    @Test
+    public void ploan(){
+        System.out.println("Test Sozinho class test01");
+    }
+}
